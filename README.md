7ea999514310a2d5e33bd60cd5e6ebf232567d56
Andrew Romanov <andrew.n.romanov@gmail.com>
2017-04-05 04:56:04 +0300

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# clojure-applied-book
+Reading Vandgrift &amp; Miller's wonderful book, Clojure Applied
+
+## REPL
+Journey through namespases
+```clojure
+(in-ns 'clojure-applied-book.ch1.validate)
+```
---
+++ test/clojure_applied_book/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-applied-book.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-applied-book.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
